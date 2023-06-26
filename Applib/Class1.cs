d20a9e3c0ab62b8339557611b8f2c6da101653be
Mwesigwa <mwesigwai433@gmail.com>
2023-06-26 07:18:14 +0300

    WHolesome commit of the version

---
+++ Applib/Class1.cs
@@ -0,0 +1,22 @@
+﻿namespace Applib;
+public class Class1
+{
+    public static bool trueItemsNum;
+    public static bool falseItemsNum;
+    public static int numOfItems;
+    public static int result;
+    public static int i;
+    public static int totalAmount =0;
+        public static int Items = 0;
+
+    public static void CalculateTotal(int[]prices)
+    {
+        int totalAmount = 0;
+        foreach (int price in prices)
+        {
+            totalAmount += price;   
+        }
+        System.Console.WriteLine("Total amount of the items is {0} ", totalAmount);
+    }
+}
+
---
+++ FirstConsole/Program.cs
@@ -0,0 +1,35 @@
+﻿using Applib;
+
+Class1.falseItemsNum = Class1.result < 0 && !Class1.trueItemsNum;
+do
+{
+    System.Console.WriteLine("Enter number of items bought");
+    string ItemsNum = Console.ReadLine();
+    Class1.trueItemsNum = int.TryParse(ItemsNum, out Class1.result);
+    if (Class1.trueItemsNum && Class1.result > 0) 
+{
+    Class1.numOfItems = Class1.result;
+    if (Class1.trueItemsNum)
+    {
+        break;
+    }
+}else
+{
+    System.Console.WriteLine("Number of items must above 0 and must be a number.");
+}
+}while (!Class1.falseItemsNum);
+
+int[] AmountForEveryItem = new int[Class1.result + 1];
+System.Console.WriteLine("Enter amount for every item");
+for (Class1.i  = 0; Class1.i < Class1.numOfItems; Class1.i++)
+{
+    Class1.Items += 1;
+    System.Console.WriteLine("Item {0}: ", Class1.Items);
+    AmountForEveryItem[Class1.i] = int.Parse(Console.ReadLine());
+    Class1.totalAmount += AmountForEveryItem[Class1.i];
+}
+System.Console.WriteLine("\n Total = " + Class1.totalAmount);
+/*for (int i = 0; i < Class1.numOfItems; i++)
+{
+   System.Console.WriteLine(AmountForEveryItem[i]); 
+}*/
