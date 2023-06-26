813fee7099fe69fac9c82a878800fd5b7a315d20
Mwesigwa <mwesigwai433@gmail.com>
2023-06-26 11:02:22 +0300

    Simple commit

--- Applib/Class1.cs
+++ Applib/Class1.cs
@@ -6,7 +6,7 @@ public class Class1
     public static int numOfItems;
     public static int result;
     public static int i;
-    public static int totalAmount =0;
+    public static int totalAmount = 0;
         public static int Items = 0;
 
     public static void CalculateTotal(int[]prices)
